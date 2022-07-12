572f576c89cf17073f8d974855196171c07139fa
772vjrvj <42423955+772vjrvj@users.noreply.github.com>
2022-07-12 23:37:18 +0900

    j
    j

--- javascript/javascript-study-772vjrvj-webSite/Test/src/test/practice.js
+++
@@ -1,9 +0,0 @@
-var Person = /** @class */ (function () {
-    function Person(name) {
-        this.name = name;
-    }
-    return Person;
-}());
-var p1 = new Person('Lee1');
-var p2 = new Person('Lee2');
-console.log('test');
