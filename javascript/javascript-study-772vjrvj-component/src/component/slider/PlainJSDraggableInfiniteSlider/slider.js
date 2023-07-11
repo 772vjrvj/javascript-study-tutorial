5bed90e4a4414815553e49809a460a08619621a8
772vjrvj <42423955+772vjrvj@users.noreply.github.com>
2023-07-12 01:08:44 +0900

    g
    gg

--- javascript/javascript-study-772vjrvj-component/src/component/slider/PlainJSDraggableInfiniteSlider/slider.js
+++ javascript/javascript-study-772vjrvj-component/src/component/slider/PlainJSDraggableInfiniteSlider/slider.js
@@ -17,7 +17,9 @@ function slide(wrapper, items, prev, next) {
         cloneFirst = firstSlide.cloneNode(true),
         cloneLast = lastSlide.cloneNode(true),
         index = 0,
-        allowShift = true;
+        allowShift = true,
+        silderLeft = wrapper.getBoundingClientRect().left, //절대 좌표
+        isLock = false;
 
     // Clone first and last slide
     items.appendChild(cloneFirst);
@@ -40,16 +42,18 @@ function slide(wrapper, items, prev, next) {
     items.addEventListener('transitionend', checkIndex);
 
     function dragStart (e) {
-        e = e || window.event;
-        e.preventDefault();
-        posInitial = items.offsetLeft;
-
-        if (e.type == 'touchstart') {
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX1 = e.clientX;
-            document.onmouseup = dragEnd;
-            document.onmousemove = dragAction;
+        if(!isLock){
+            e = e || window.event;
+            e.preventDefault();
+            posInitial = items.offsetLeft;
+    
+            if (e.type == 'touchstart') {
+                posX1 = e.touches[0].clientX;
+            } else {
+                posX1 = e.clientX;
+                document.onmouseup = dragEnd;
+                document.onmousemove = dragAction;
+            }
         }
     }
 
@@ -62,15 +66,32 @@ function slide(wrapper, items, prev, next) {
         } else {
             posX2 = posX1 - e.clientX;
             posX1 = e.clientX;
+            if(e.clientX >= silderLeft && e.clientX <= silderLeft + slideSize){
+                items.style.left = (items.offsetLeft - posX2) + "px";
+            }
         }
-        items.style.left = (items.offsetLeft - posX2) + "px";
     }
 
     function dragEnd (e) {
         posFinal = items.offsetLeft;
+
+        //마지막 위치와 시작 위치의 절대값 차이가 100이 넘어야 한다.
+        //|posFinal - posInitial| > 100
+        // (-400 + -101)  - (-400) < -(100) 
+        // 1/4만큼 왼쪽으로 더 가야 이동 ... 
         if (posFinal - posInitial < -threshold) {
+            console.log('1');
+            //오른쪽 방향
+            //숫자는 감소
             shiftSlide(1, 'drag');
+
+
+            // (-400 + + 101)  - (-400) > (100) 
+            // 1/4만큼 오른쪽로 더 가야 이동 ... 
         } else if (posFinal - posInitial > threshold) {
+            console.log('2');
+            //오른쪽 방향
+            //숫자는 감소
             shiftSlide(-1, 'drag');
         } else {
             items.style.left = (posInitial) + "px";
@@ -86,10 +107,15 @@ function slide(wrapper, items, prev, next) {
         if (allowShift) {
             if (!action) { posInitial = items.offsetLeft; }
 
+            //왼족버튼
             if (dir == 1) {
+                isLock = true;
                 items.style.left = (posInitial - slideSize) + "px";
                 index++;
+
+            //오른쪽 버튼
             } else if (dir == -1) {
+                isLock = true;
                 items.style.left = (posInitial + slideSize) + "px";
                 index--;
             }
@@ -101,17 +127,20 @@ function slide(wrapper, items, prev, next) {
     function checkIndex (){
         items.classList.remove('shifting');
 
+        //오른쪽 이동 (숫자 감소) 하면서 slide가 1번에서 끝번호 5번이으로 온경우 5번은 복사 본이므로 원본으로 이동시켜야함
         if (index == -1) {
             items.style.left = -(slidesLength * slideSize) + "px";
             index = slidesLength - 1;
         }
 
+        //왼쪽으로 읻홍(숫자 증가) 하면서 slide가 5에서 1(복사본)로 간경우
         if (index == slidesLength) {
             items.style.left = -(1 * slideSize) + "px";
             index = 0;
         }
 
         allowShift = true;
+        isLock = false;
     }
 }
 
